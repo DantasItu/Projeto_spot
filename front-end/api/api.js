1958eafb0019a42fe9487efe8dd3d9bd9cad06d9
unknown <michael.of.douglasaraujodantas5@gmail.com>
2025-02-25 23:33:02 -0300

    gggg

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -4,7 +4,10 @@ import axios from "axios";
 
 const { NODE_ENV } = process.env;
 
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+const URL =
+  NODE_ENV === "development"
+    ? "http://localhost:3001/api"
+    : "https://projeto-spot.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
