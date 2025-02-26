ddd47bbb2b1d9aac70e25948d3cfaaf8414c7277
unknown <michael.of.douglasaraujodantas5@gmail.com>
2025-02-26 01:11:28 -0300

    adicionei a pasta song com um mp3 para test

--- back-end/api/insertMany.js
+++ back-end/api/insertMany.js
@@ -1,17 +1,17 @@
 import { artistArray } from "../../front-end/src/assets/database/artists.js";
-import { songsArray } from "../../front-end/src/assets/database/songs.js";
+import { songsArray2 } from "../../front-end/src/assets/database/temple.js";
 import { db } from "./connect.js";
 
 // newArtistsArray
-const newArtistArray = artistArray.map((currArtistOj) => {
-  const newArtistObj = { ...currArtistOj };
-  delete newArtistObj.id;
+// const newArtistArray = artistArray.map((currArtistOj) => {
+//   const newArtistObj = { ...currArtistOj };
+//   delete newArtistObj.id;
 
-  return newArtistObj;
-});
+//   return newArtistObj;
+// });
 
 // newSongsArray
-const newsongsArray = songsArray.map((currsongsOj) => {
+const newsongsArray = songsArray2.map((currsongsOj) => {
   const newsongsObj = { ...currsongsOj };
 
   return newsongsObj;
@@ -19,9 +19,9 @@ const newsongsArray = songsArray.map((currsongsOj) => {
 
 const responseSongs = await db.collection("songs").insertMany(newsongsArray);
 
-const responseArtists = await db
-  .collection("artists")
-  .insertMany(newArtistArray);
+// const responseArtists = await db
+//   .collection("artists")
+//   .insertMany(newArtistArray);
 
 console.log(responseSongs);
-console.log(responseArtists);
+// console.log(responseArtists);
