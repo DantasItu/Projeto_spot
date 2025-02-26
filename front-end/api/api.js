ae6a316535082efa574b34db3443a3aae41de24b
unknown <michael.of.douglasaraujodantas5@gmail.com>
2025-02-25 23:38:47 -0300

    ajuste no prosses env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,12 +2,14 @@
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
+// const { NODE_ENV } = process.env;
 
-const URL =
-  NODE_ENV === "development"
-    ? "http://localhost:3001/api"
-    : "https://projeto-spot.onrender.com/api";
+// const URL =
+//   NODE_ENV === "development"
+//     ? "http://localhost:3001/api"
+//     : "/api";
+
+const URL = "https://projeto-spot.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
